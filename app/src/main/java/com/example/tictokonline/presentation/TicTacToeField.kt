2b7f343bb58df2e3a1d0e7cd0d4c5d54ce22419f
Khalid <khalidwork050@outlook.com>
2024-01-05 14:10:04 +0400

    add logic for showing the x and o depend on the field of the game state

--- app/src/main/java/com/example/tictokonline/presentation/TicTacToeField.kt
+++ app/src/main/java/com/example/tictokonline/presentation/TicTacToeField.kt
@@ -1,16 +1,16 @@
 package com.example.tictokonline.presentation
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tictokonline.data.GameState
@@ -25,13 +25,27 @@ fun TicTacToeField(
     onTapInField: (x: Int, y: Int) -> Unit
 ) {
     Canvas(modifier = modifier) {
+        // draw the lines vertical and horizontal
         drawVerticalLine(size.width * 1 / 3)
         drawVerticalLine(size.width * 2 / 3)
         drawHorizontalLine(size.height * 1 / 3)
         drawHorizontalLine(size.height * 2 / 3)
 
-         drawX(color = Color.Red ,  center = Offset(size.width * (1/ 6f) , size.height *(5/ 6f)))
-         drawO(color = Color.Green , center = Offset(x = size.width /6 , y = size.height/2))
+        //draw the x and o depend on the game field array
+        state.field.forEachIndexed { x , array ->
+            array.forEachIndexed { y , char->
+                val offset = offsetFormal(x, y)
+                if (char == 'X') {
+                    drawX(color = playerXColor , center= offset)
+                } else if (char == 'O') {
+                    drawO(color =  playerOColor , center= offset)
+                } else {
+                    //
+                }
+            }
+        }
+
+
     }
 }
 
@@ -114,6 +128,19 @@ private fun DrawScope.drawHorizontalLine(startY: Float) {
     )
 }
 
+/**
+ *  this will give the formal for set x and o in correct position in the whiteBoard
+ */
+private fun DrawScope.offsetFormal(
+    x: Int,
+    y: Int
+): Offset {
+    return Offset(
+        x = size.width * (1 / 3f * y) + size.width / 6f,
+        y = size.height * (1 / 3f * x) + size.height / 6f
+    )
+}
+
 
 @Preview(showBackground = true)
 @Composable
@@ -122,7 +149,7 @@ fun PreviewTicTocToeField() {
     TicTacToeField(
         state = GameState(
             field = arrayOf(
-                arrayOf( 'O'  , null , 'X'),
+                arrayOf( 'O'  , 'X' , 'O'),
                 arrayOf( null , null , null),
                 arrayOf( null , null , null)
             )
